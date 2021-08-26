de6eda7ac181562e90d3be6c5e3e45ef0ebb4f51
enkog <nkirukaogu@gmail.com>
2021-08-26 10:41:39 +0100

    Create tests to add a list element to the list in the DOM

--- src/__tests__/taskUtils.test.js
+++ src/__tests__/taskUtils.test.js
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import TaskUtils from '../taskUtils.js';
 
 jest.mock('../taskUtils.js');
@@ -5,14 +9,33 @@ jest.mock('../taskUtils.js');
 const taskUtils = new TaskUtils();
 
 document.body.innerHTML = '<div>'
-    + '  <ul id="list"></li>'
+    + '  <ul id="list"></ul>'
     + '</div>';
 
+const displayTodo = (arr) => {
+  const ul = document.getElementById('list');
+  arr.forEach(() => {
+    const li = document.createElement('li');
+    ul.appendChild(li);
+  });
+};
 describe('add exactly one <li> element to the list in the DOM', () => {
-  test('adding new input', () => {
-    const tasks = taskUtils.getTasks();
-    const task = 2;
-    taskUtils.addTask(task);
-    expect(tasks).toContain(task);
+  const task = { description: 'Task 1', completed: false, index: 1 };
+  taskUtils.addTask(task);
+  const tasks = taskUtils.getTasks();
+  displayTodo(tasks);
+
+  test('that the length of the list is 1', () => {
+    const list = document.querySelectorAll('#list li');
+    expect(list).toHaveLength(1);
+  });
+
+  test('that the length of the list is not zero', () => {
+    const list = document.querySelectorAll('#list li');
+    expect(list).not.toHaveLength(0);
+  });
+
+  test('that the description is Task 1', () => {
+    expect(tasks[0].description).toEqual('Task 1');
   });
 });
