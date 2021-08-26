69bae888d6e937b844a13c0141783edc4a320b60
ali abbani <aliox@ahlam.com>
2021-08-26 11:28:22 +0300

    Add first test function for the add task

--- src/__mocks__/taskUtils.js
+++ src/__mocks__/taskUtils.js
@@ -1,6 +1,9 @@
 const tasks = [];
 
 const mock = jest.fn().mockImplementation(() => ({
+  getTasks() {
+    return tasks;
+  },
   addTask(task) {
     tasks.push(task);
   },
--- src/__tests__/taskUtils.test.js
+++ src/__tests__/taskUtils.test.js
@@ -1,3 +1,11 @@
 import TaskUtils from '../taskUtils.js';
 
 jest.mock('../taskUtils.js');
+const taskUtils = new TaskUtils();
+
+test('adding new input', () => {
+  const tasks = taskUtils.getTasks();
+  const task = 2;
+  taskUtils.addTask(task);
+  expect(tasks).toContain(task);
+});
