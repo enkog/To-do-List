5da32527ad8b2e915d510741712c2cfc6f79f72b
enkog <nkirukaogu@gmail.com>
2021-08-26 10:26:27 +0100

    Add mock for DOM manipulation

--- src/__tests__/taskUtils.test.js
+++ src/__tests__/taskUtils.test.js
@@ -1,11 +1,18 @@
 import TaskUtils from '../taskUtils.js';
 
 jest.mock('../taskUtils.js');
+
 const taskUtils = new TaskUtils();
 
-test('adding new input', () => {
-  const tasks = taskUtils.getTasks();
-  const task = 2;
-  taskUtils.addTask(task);
-  expect(tasks).toContain(task);
+document.body.innerHTML = '<div>'
+    + '  <ul id="list"></li>'
+    + '</div>';
+
+describe('add exactly one <li> element to the list in the DOM', () => {
+  test('adding new input', () => {
+    const tasks = taskUtils.getTasks();
+    const task = 2;
+    taskUtils.addTask(task);
+    expect(tasks).toContain(task);
+  });
 });
