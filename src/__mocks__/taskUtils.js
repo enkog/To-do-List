b5ac78a2c1ffe0b6eb9bac7f319f87e56edc54f7
enkog <nkirukaogu@gmail.com>
2021-08-26 10:56:23 +0100

    Add mock function for deleting a task

--- src/__mocks__/taskUtils.js
+++ src/__mocks__/taskUtils.js
@@ -1,11 +1,26 @@
-const tasks = [];
+let tasks = [];
 
 const mock = jest.fn().mockImplementation(() => ({
   getTasks() {
     return tasks;
   },
+  setItems(arr) {
+    tasks = [...arr];
+  },
   addTask(task) {
     tasks.push(task);
+  },
+  deleteTask(index) {
+    const filteredTasks = tasks.filter((task, i) => i !== index);
+    const indexedTasks = this.reIndexTasks(filteredTasks);
+    this.setItems(indexedTasks);
+  },
+  reIndexTasks(todos) {
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < todos.length; i++) {
+      todos[i].index = i + 1;
+    }
+    return todos;
   },
 }));
 
