9981ec55d6320cc46051e078570c9c88b197ea40
enkog <nkirukaogu@gmail.com>
2021-08-27 22:11:00 +0100

    Add a .js and .test.js file for the completed function

---
+++ src/__mocks__/completed.js
@@ -0,0 +1,3 @@
+export default function completed(task) {
+  task.completed = !task.completed;
+}
--- src/__mocks__/taskUtils.js
+++ src/__mocks__/taskUtils.js
@@ -4,9 +4,6 @@ const mock = jest.fn().mockImplementation(() => ({
   getTasks() {
     return tasks;
   },
-  completed(task) {
-    task.completed = !task.completed;
-  },
   clearCompleted() {
     return tasks.filter((task) => task.completed === false);
   },
